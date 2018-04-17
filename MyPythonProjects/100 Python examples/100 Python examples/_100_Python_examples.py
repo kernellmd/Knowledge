aa1a7fac81883158dce9a25ce233a4a5cc9a7df8
kernellmd <KernelLmd@163.com>
2018-04-17 20:02:30 +0800

    example-1 改进

--- MyPythonProjects/100 Python examples/100 Python examples/_100_Python_examples.py
+++ MyPythonProjects/100 Python examples/100 Python examples/_100_Python_examples.py
@@ -2,6 +2,19 @@
 题目描述：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
 程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后，再去掉不满足条件的排列。
 """
+#改进1
+num = 0
+for hundreds in range(1, 5):
+    for tens in range(1, 5):
+            for ones in range(1, 5):
+                if (hundreds != tens) and \
+                   (tens != ones) and (hundreds != ones): #取三个不同的数字
+                    print(hundreds * 100 + tens * 10 + ones)
+                    num += 1
+                    
+print(num)
+'''
+#原始程序
 num = 0
 for hundreds in range(1, 5):
     var = hundreds * 100
@@ -22,3 +35,4 @@ for hundreds in range(1, 5):
         else:
             continue
 print(num)
+'''
