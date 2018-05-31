b41412a9b1531edd0805ca08dc9ce1d6dddac8de
kernellmd <KernelLmd@163.com>
2018-05-31 22:40:44 +0800

    add MyPythonProjects/MySpider

---
+++ MyPythonProjects/MySpider/spider_framework.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-  
+############################  
+#File Name: spider_framework.py
+#Author: kernellmd 
+#Created Time: 2018-05-31 22:21:29
+############################  
+
+import requests
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()  #如果状态不是200，引发HTTPError异常
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "异常产生"
+
+if __name__ == "__main__":
+    url = "http://www.baidu.com"
+    print(getHTMLText(url))
