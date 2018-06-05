e370448ca22939905af20c7b26d69975b3b416b3
kernellmd <KernelLmd@163.com>
2018-06-05 19:32:45 +0800

    modify a little

--- MyPythonProjects/MySpider/BeautifulSoupDemo.py
+++ MyPythonProjects/MySpider/BeautifulSoupDemo.py
@@ -13,4 +13,28 @@ soup = BeautifulSoup(demo, 'html.parser')
 #print(soup.title)
 #获取页面的a标签
 #print(soup.a)
-print(soup.prettify())
+#打印
+#prettify()函数可以为html文本增加换行符，也可以单独对标签进行处理
+#print(soup.prettify())
+#获取head标签
+#print(soup.head)
+#获取head标签的子标签节点
+#.contents返回一个列表，此列表不仅包括子标签，还包括字符串如'\n'
+#print(soup.head.contents)
+#body标签
+#body_children = soup.body.contents
+#print(body_children)
+#print(body_children[1])
+#soup的父标签就是他本身
+#print(soup.html)
+#print(soup.html.parent)
+
+#a标签的平行标签
+a_tag = soup.a
+print(a_tag.prettify())
+#print(a_tag.next_sibling)
+#print(a_tag.next_sibling.next_sibling)
+
+#print()
+#print(a_tag.previous_sibling)
+#print(a_tag.previous_sibling.previous_sibling)  #为空
--- MyPythonProjects/MySpider/spider_framework.py
+++ MyPythonProjects/MySpider/spider_framework.py
@@ -15,8 +15,8 @@ def getHTMLText(url):
         r.raise_for_status()  #如果状态不是200，引发HTTPError异常
         r.encoding = r.apparent_encoding
         return r.text
-    except:
-        return "异常产生"
+    except Exception as e:
+        return (type(e), e)
 
 if __name__ == "__main__":
     url = "http://www.baidu.com"
