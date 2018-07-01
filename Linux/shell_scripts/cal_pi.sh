7a19bac6a51332b16650965545d2ad4a0f8e4d45
kernellmd <KernelLmd@163.com>
2018-07-01 21:52:02 +0800

    modify cal_pi.sh

--- Linux/shell_scripts/cal_pi.sh
+++ Linux/shell_scripts/cal_pi.sh
@@ -12,3 +12,4 @@ read -p "The scale number (10~10000) ? " checking
 num=${checking:-"10"}           # 開始判斷有否有輸入數值
 echo -e "Starting calculate pi value.  Be patient."
 time echo "scale=${num}; 4*a(1)" | bc -lq
+exit 0
