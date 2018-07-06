9c4f4ea2c62dd1758599db1338d422ba0c385295
kernellmd <KernelLmd@163.com>
2018-07-06 12:49:35 +0800

    add C_C++Projects/cmd_menu/menu menu.c

---
+++ C_C++Projects/cmd_menu/menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+int main(){
+	char cmd[100];
+	while(1){
+		scanf("%s", cmd);
+		//if(!strcmp(cmd, "help")) 易读性不好
+		if(strcmp(cmd, "help") == 0){
+			printf("This is help cmd!\n");
+		}
+		else if(strcmp(cmd, "quit") == 0){
+			exit(0);
+		}
+		else{
+			printf("WRONG CMD!\n");
+		}
+	}
+
+	return 0;
+}
