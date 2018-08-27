a8ac03650738792a876b7697c8bd9893d75deb78
kernellmd <KernelLmd@163.com>
2018-08-27 17:49:01 +0800

    add *

---
+++ PythonProjects/OOP/vigenere_password_app/test_program.py
@@ -0,0 +1,6 @@
+from vigenere_cipher import VigenereCipher
+
+def test_encode():
+    cipher = VigenereCipher("TRAIN")
+    encoded = cipher.encode("ENCODEDINPYTHON")
+    assert encoded == "XECWQXUIVCRKHWA"
---
+++ PythonProjects/OOP/vigenere_password_app/vigenere_cipher.py
@@ -0,0 +1,9 @@
+class VigenereCipher:
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+    def encode(self, plaintext):
+        return "XECWQXUIVCRKHWA"
+
+
+
