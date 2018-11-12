2c6f1e612b012cc19065106868488b6d96f56d41
kernellmd <KernelLmd@163.com>
2018-11-12 22:11:05 +0800

    add some notes about js

--- JSProjects/scripts/example.js
+++ JSProjects/scripts/example.js
@@ -4,7 +4,30 @@
  * 块级注释
  */
 
+var message = "Hello World!"
+var message2
+var bool_test = null;
+var testAsBoolean = Boolean(bool_test);
+var result = Number.MAX_VALUE + Number.MAX_VALUE;
+
+
 function sayHi() {
-    alert("Hi!");
+    alert(Number.NEGATIVE_INFINITY);
+    alert(Number.POSITIVE_INFINITY);
+    //alert(isFinite(result));
+    //alert(Number.MAX_VALUE);
+    //alert(Number.MIN_VALUE);
+    //if (message) {  //自动执行Boolean()函数
+    //    alert("Value is ture");
+    //}
+    //alert(testAsBoolean);
+    //alert(typeof hello);  //hello变量为声明,"undefined"
+    //alert(typeof message2);  //message2为未初始化的变量,"undefined"
+    //alert(typeof message);  //typeof是一个操作符而不是函数，所以可以不使用括号
+    //alert(typeof(message));  //"string"
+    //alert(typeof(59))   //"number"
+    //alert(typeof (null));  //"object"
+    //alert(typeof sayHi);  //"function"
 }
+
 window.onload = sayHi();
