b187e73f4ca7fa2c7597e3f6b6ee122ebd255107
kernellmd <KernelLmd@163.com>
2019-03-28 14:01:27 +0800

    delete OOP and Spyder code

--- PythonProjects/OOP/vigenere_password_app/test_encode_keyword_method.py
+++
@@ -1,6 +0,0 @@
-from vigenere_cipher import VigenereCipher
-
-def test_extend_keyword():
-    cipher = VigenereCipher("TRAIN")
-    extended = cipher.extend_keyword(16)
-    assert extended == "TRAINTRAINTRAINT"
